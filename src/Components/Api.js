96fe317ad7e3bea2b4927b047c88b3f3db19fde5
Dave Longbottom <davelongbottom@gmail.com>
2018-12-14 07:56:55 +1030

    added api (random user) fetch

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -2,10 +2,50 @@ import './Api.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Background extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			pictures: [],
+		};
+	}
+	componentWillMount() {
+
+		fetch('https://randomuser.me/api/?results=100')
+		.then(results => {
+			return results.json();
+		})
+		.then(data =>{
+			let pictures = data.results.map((pic) => {
+				return (
+					<div key={pic.cell}> {/*changed from 'pic.results' (tutorial) to 'pic.cell' (currently available in Api)*/}
+						<img src={pic.picture.medium} />
+					</div>
+				)
+			})
+			this.setState({pictures: pictures});
+			console.log("state", this.state.pictures);
+			//console.log("state", data.results);
+		})
+	}
+
+	render() {
+	return(
+		<div className="container2">
+			<div className="container1">
+				{this.state.pictures}
+			</div>
+		</div>
+		);
+	}
+}
+
+
 class Api extends React.Component {
   render() {
     return (
-      <h1>Hello Api</h1>
+      <Background />
       );
   }
 }

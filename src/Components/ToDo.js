d32dc1aad72e5027ac4016b04b1454a7b7059cda
Dave Longbottom <davelongbottom@gmail.com>
2018-12-17 13:13:54 +1030

    added 'add to list' functionality

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -7,10 +7,10 @@ import ReactDOM from 'react-dom';
 // so while the state that seeds from it can be updated, this seed data can not mutate
 
 const initData = [
-	{id: 0, description: "Fix the washing machine" },
-	{id: 1, description: "Get food for the dog" },
-	{id: 2, description: "Trim the weeds" },
-	{id: 3, description: "Paint the fence" },
+	{id: 0, description: "Fix the washing machine", selected:false },
+	{id: 1, description: "Get food for the dog", 	selected:false },
+	{id: 2, description: "Trim the weeds",			selected:false },
+	{id: 3, description: "Paint the fence", 		selected:false },
 ];
 
 function Heading(props) {
@@ -23,12 +23,40 @@ class ItemRow extends React.Component {
 				<li>
 					<label className="container">
 					  <input type="checkbox" checked={this.props.checked} />
-					  <span className="lable">{this.props.description}</span>
+					  <span className="lable">
+					   <span className="list-number">
+					   {this.props.id+1}
+					   </span>
+					   {this.props.description}
+					  </span>
 					</label>
 				</li>
 			);
 	}
 
+}
+class InputAddItem extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+				<div>
+				<input
+				type="text"
+				value={this.props.value}
+				onChange={this.props.onChange}
+				/>
+				<button 
+				onClick={this.props.onClick}
+				>
+				Add To List
+				</button>
+				</div>
+			);
+	}
 }
 
 class ToDoList extends React.Component {
@@ -38,17 +66,46 @@ class ToDoList extends React.Component {
 		super(props);
 		this.state = {
 			items: initData, 
+			itemToAdd: '',
 		};
+		this.handleClick = this.handleClick.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleClick(e) {
+
+		// add item
+
+		this.setState({
+			//copy items (slice) and append (concat) the new object 
+			items: this.state.items.slice().concat(
+				[{
+					id: this.state.items.length, 
+					description: this.state.itemToAdd 
+				}]
+				),
+			itemToAdd: '', // reset input value
+		});
+	}
+
+	handleChange(e) {
+		this.setState({itemToAdd: e.target.value});
 	}
 
 	render() {
 		return(
 			<div className="to-do-list">
 				<Heading title="My To Do List" />
+				<InputAddItem 
+				value={this.state.itemToAdd} 
+				onChange={this.handleChange}
+				onClick={this.handleClick}
+				/>
 				<ul className="item-list">
 				{this.state.items.map((item) => 
 						<ItemRow 
 						key={item.id}
+						id={item.id}
 						description={item.description} 
 						checked={item.checked} 
 						/>

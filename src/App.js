777974a9b800bde9db86a76c9c89f70182f4289d
Dave Longbottom <davelongbottom@gmail.com>
2018-12-18 12:11:55 +1030

    added files for Calculator Component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FreeCodeCamp from './Components/FreeCodeCamp';
 import HelloWorld from './Components/HelloWorld';
 import Api from './Components/Api';
 import ToDo from './Components/ToDo';
+import Calculator from './Components/Calculator';
 
 const data = [
   {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
@@ -23,11 +24,12 @@ const appComponents = {
  freeCodeCamp : <FreeCodeCamp />,
  api : <Api />,
  toDo : <ToDo />,
+ calculator : <Calculator />,
 }
 
 // set runApp to the appComponents key name above that you want to run
 
-const runApp = "toDo";
+const runApp = "calculator";
 
 class App extends Component {
   render() {
---
+++ src/Components/Calculator.js
@@ -0,0 +1,14 @@
+import './Calculator.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <h1>Hello Calculator</h1>
+      );
+  }
+}
+
+
+export default Calculator;

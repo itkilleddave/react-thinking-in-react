bcb68cc3893dcfa4bd70f3bd537745a266f07308
Dave Longbottom <davelongbottom@gmail.com>
2018-12-20 07:49:16 +1030

    added shop

--- src/App.js
+++ src/App.js
@@ -8,29 +8,31 @@ import HelloWorld from './Components/HelloWorld';
 import Api from './Components/Api';
 import ToDo from './Components/ToDo';
 import Calculator from './Components/Calculator';
+import Shop from './Components/Shop';
 
-const data = [
-  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+const products = [
+  {category: "Sporting Goods", price: "49.99", stocked: true, name: "Football"},
+  {category: "Sporting Goods", price: "9.99", stocked: true, name: "Baseball"},
+  {category: "Sporting Goods", price: "29.99", stocked: false, name: "Basketball"},
+  {category: "Electronics", price: "99.99", stocked: true, name: "iPod Touch"},
+  {category: "Electronics", price: "399.99", stocked: false, name: "iPhone 5"},
+  {category: "Electronics", price: "199.99", stocked: true, name: "Nexus 7"}
 ];
 
 const appComponents = {
  helloWorld : <HelloWorld />,
- searchableProductTable : <SearchableProductTable products={data} />,
+ searchableProductTable : <SearchableProductTable products={products} />,
  ticTacToe : <TicTacToe />,
  freeCodeCamp : <FreeCodeCamp />,
  api : <Api />,
  toDo : <ToDo />,
  calculator : <Calculator />,
+ shop : <Shop products={products} />,
 }
 
 // set runApp to the appComponents key name above that you want to run
 
-const runApp = "helloWorld";
+const runApp = "shop";
 
 class App extends Component {
   render() {

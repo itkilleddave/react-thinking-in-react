5393894f69f5f8b8790d279e635b8a9f79b09d71
Dave Longbottom <davelongbottom@gmail.com>
2019-01-03 19:37:37 +1030

    rows and columns

--- src/App/CssGrid/CssGrid.js
+++ src/App/CssGrid/CssGrid.js
@@ -1,10 +1,22 @@
 import './CssGrid.css';
+import './1.css';
 import React from 'react';
 
 class CssGrid extends React.Component {
   render() {
     return (
-      <h1>Hello CssGrid</h1>
+      <div className="container">
+      	<div className="item">1</div>
+      	<div className="item">2</div>
+      	<div className="item">3</div>
+      	<div className="item">4</div>
+      	<div className="item">5</div>
+      	<div className="item">6</div>
+      	<div className="item">7</div>
+      	<div className="item">8</div>
+      	<div className="item">9</div>
+      	<div className="item">10</div>
+      </div>
       );
   }
 }

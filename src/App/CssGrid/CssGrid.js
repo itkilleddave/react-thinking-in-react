caf5d7927e6cce1ea503c856193633ae09dafd89
Dave Longbottom <davelongbottom@gmail.com>
2019-01-04 08:00:28 +1030

    auto-fit and auto-fill

--- src/App/CssGrid/CssGrid.js
+++ src/App/CssGrid/CssGrid.js
@@ -6,31 +6,31 @@ class CssGrid extends React.Component {
   render() {
     return (
       <div className="container">
-      	<div className="item item1">1</div>
-      	<div className="item item2">2</div>
-      	<div className="item item3">3</div>
-      	<div className="item item4">4</div>
-      	<div className="item item5">5</div>
-      	<div className="item item6">6</div>
-      	<div className="item item7">7</div>
-      	<div className="item item8">8</div>
-      	<div className="item item9">9</div>
-      	<div className="item item10">10</div>
-      	<div className="item item11">11</div>
-      	<div className="item item12">12</div>
-      	<div className="item item13">13</div>
-      	<div className="item item14">14</div>
-      	<div className="item item15">15</div>
-      	<div className="item item16">16</div>
-      	<div className="item item17">17</div>
-      	<div className="item item18">18</div>
-      	<div className="item item19">19</div>
-      	<div className="item item20">20</div>
-      	<div className="item item21">21</div>
-      	<div className="item item22">22</div>
-      	<div className="item item23">23</div>
-      	<div className="item item24">24</div>
-      	<div className="item item25">25</div>
+      	<div className="item item1">Item 01</div>
+      	<div className="item item2">Item 02</div>
+      	<div className="item item3">Item 03</div>
+      	<div className="item item4">Item 04</div>
+{/*      	<div className="item item5">Item 05</div>
+      	<div className="item item6">Item 06</div>
+      	<div className="item item7">Item 07</div>
+      	<div className="item item8">Item 08</div>
+      	<div className="item item9">Item 09</div>
+      	<div className="item item10">Item 10</div>
+      	<div className="item item11">Item 11</div>
+      	<div className="item item12">Item 12</div>
+      	<div className="item item13">Item 13</div>
+      	<div className="item item14">Item 14</div>
+      	<div className="item item15">Item 15</div>
+      	<div className="item item16">Item 16</div>
+      	<div className="item item17">Item 17</div>
+      	<div className="item item18">Item 18</div>
+      	<div className="item item19">Item 19</div>
+      	<div className="item item20">Item 20</div>
+      	<div className="item item21">Item 21</div>
+      	<div className="item item22">Item 22</div>
+      	<div className="item item23">Item 23</div>
+      	<div className="item item24">Item 24</div>
+      	<div className="item item25">Item 25</div>*/}
       </div>
       );
   }

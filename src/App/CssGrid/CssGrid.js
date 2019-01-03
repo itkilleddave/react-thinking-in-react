9df5b14a6089e8923acbb0331eab5e891fecbd39
Dave Longbottom <davelongbottom@gmail.com>
2019-01-03 20:27:23 +1030

    fr units

--- src/App/CssGrid/CssGrid.js
+++ src/App/CssGrid/CssGrid.js
@@ -8,7 +8,7 @@ class CssGrid extends React.Component {
       <div className="container">
       	<div className="item">1</div>
       	<div className="item">2</div>
-      	<div className="item">3</div>
+      	<div className="item">sadsad saadas dassa</div>
       	<div className="item">4</div>
       	<div className="item">5</div>
       	<div className="item">6</div>
@@ -16,6 +16,11 @@ class CssGrid extends React.Component {
       	<div className="item">8</div>
       	<div className="item">9</div>
       	<div className="item">10</div>
+      	<div className="item">11</div>
+      	<div className="item">12</div>
+      	<div className="item">13</div>
+      	<div className="item">14</div>
+      	<div className="item">15</div>
       </div>
       );
   }
